4e9e9cb351596afd811a982f163adf45c8ae9901
Mohsen Shakibafar <Shakibafar@mapnaec.com>
2018-09-10 08:36:53 +0430

    Add project files.

---
+++ CorrectFtpCorrupt/FMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CorrectFtpCorrupt
+{
+    partial class FMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txInputPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txOutpuPath = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // txInputPath
+            // 
+            this.txInputPath.Location = new System.Drawing.Point(121, 24);
+            this.txInputPath.Name = "txInputPath";
+            this.txInputPath.Size = new System.Drawing.Size(422, 20);
+            this.txInputPath.TabIndex = 0;
+            this.txInputPath.Text = "C:\\";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartClick);
+            // 
+            // txOutpuPath
+            // 
+            this.txOutpuPath.Location = new System.Drawing.Point(121, 50);
+            this.txOutpuPath.Name = "txOutpuPath";
+            this.txOutpuPath.Size = new System.Drawing.Size(422, 20);
+            this.txOutpuPath.TabIndex = 3;
+            this.txOutpuPath.Text = "D:\\";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(954, 223);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "Press Start...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(549, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(549, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Input Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Output Directory";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(951, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // FMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 371);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txOutpuPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txInputPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Correct FTP Corruption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txInputPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txOutpuPath;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ CorrectFtpCorrupt/FMain.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorrectFtpCorrupt
+{
+    public partial class FMain : Form
+    {
+        List<string> filelist = new List<string>();
+        public FMain()
+        {
+            InitializeComponent();
+        }
+
+        private void StartClick(object sender, EventArgs e)
+        {
+            filelist = Directory.GetFiles(txInputPath.Text, "*.*", SearchOption.AllDirectories).ToList<string>();
+
+            if(filelist.Count==0)
+            {
+                MessageBox.Show("No file were found!");
+                return;
+            }
+
+            DialogResult dr = MessageBox.Show(filelist.Count.ToString()+" files were found. Do you want to continue?",
+                "Question" , MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+            if (dr == DialogResult.Yes)
+            {
+                progressBar1.Maximum = filelist.Count;
+                progressBar1.Value = 0;
+                ThreadPool.QueueUserWorkItem(RunRepaid);
+            }
+        }
+
+        private void RunRepaid(Object obj)
+        {
+            int c = 0;
+
+            foreach (var item in filelist)
+            {
+
+                try
+                {
+                    CorrectFile(item);
+                    AddText(item + " ---> is done.",c);
+                    c++;
+                }
+                catch(Exception ex)
+                {
+;                    AddText(ex.ToString(), c);
+                }
+                
+
+            }
+
+            AddText(filelist.Count.ToString() + " file have repaired",progressBar1.Maximum);
+        }
+
+        delegate void AddTextDel(string v,int c);
+
+        private void AddText(string item,int x)
+        {
+            if (richTextBox1.InvokeRequired)
+                richTextBox1.Invoke(new AddTextDel(AddText), item,x);
+            else
+            {
+                richTextBox1.AppendText( Environment.NewLine + item);
+                richTextBox1.ScrollToCaret();
+                progressBar1.Value = x;
+
+            }
+        }
+
+        private void CorrectFile(string fname)
+        {
+            FileInfo fi = new FileInfo(fname);
+            string OutFile = txOutpuPath.Text + "\\"+
+                fi.DirectoryName.Replace("\\","_").Replace(":","_")+fi.Name;
+            byte[] fileBYtes = File.ReadAllBytes(fname);
+
+           // System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            string fileAsString = Encoding.Default.GetString(fileBYtes);
+
+            // textBox2.Text = fileAsString;
+            fileAsString = fileAsString.Replace("\r\n", "\n");
+           // MessageBox.Show(CountSubstring(fileAsString, "\r\n").ToString());
+
+            byte[] toBytes = Encoding.Default.GetBytes(fileAsString);
+
+            File.WriteAllBytes(OutFile, toBytes);
+          //  textBox2.Text += fname;
+           // MessageBox.Show("Test");
+           // this.Close();
+
+        }
+        public  int CountSubstring( string text, string value)
+        {
+            int count = 0, minIndex = text.IndexOf(value, 0);
+            while (minIndex != -1)
+            {
+                minIndex = text.IndexOf(value, minIndex + value.Length);
+                count++;
+            }
+            return count;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.SelectedPath = txInputPath.Text;
+            fb.ShowNewFolderButton = false;
+
+            if(fb.ShowDialog()==DialogResult.OK)
+            {
+                txInputPath.Text = fb.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.ShowNewFolderButton = true;
+            fb.SelectedPath = txOutpuPath.Text;
+
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                txOutpuPath.Text = fb.SelectedPath;
+            }
+        }
+    }
+}
